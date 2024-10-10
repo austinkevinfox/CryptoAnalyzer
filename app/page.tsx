bb0a3a770c46d04b6f36601147ea4f68efa42b2c
Kevin Fox <kevinfox.texas@proton.me>
2024-10-10 09:38:52 -0500

    Remove unused Image import

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-import Image from 'next/image'
-import ProductCard from './components/ProductCard'
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
-  return (
-      <main>
-          <h1>Cryptocurrency Analyzer</h1>
-          
-          <p>
-            We analyze and compare cryptocurrency data servered up by multiple crypo API resources, supporting our customers&apos;
-             decision making process.
-          </p>
-          
-          <ProductCard />
-      </main>
-  );
+    return (
+        <main>
+            <h1>Cryptocurrency Analyzer</h1>
+
+            <p>
+                We analyze and compare cryptocurrency data servered up by
+                multiple crypo API resources, supporting our customers&apos;
+                decision making process.
+            </p>
+
+            <ProductCard />
+        </main>
+    );
 }
